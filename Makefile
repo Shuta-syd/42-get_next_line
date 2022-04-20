af624710efacdecaa6d92d7b6ddef0887fa4cbc6
Shuta Ogura <shogura@c1r31s3.42tokyo.jp>
2022-04-20 11:49:52 +0900

    add bonus files | need to fix SEGV

--- Makefile
+++ Makefile
@@ -6,20 +6,27 @@
 #    By: shogura <shogura@student.42tokyo.jp>       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/04/09 18:43:16 by shogura           #+#    #+#              #
-#    Updated: 2022/04/19 15:11:31 by shogura          ###   ########.fr        #
+#    Updated: 2022/04/20 11:38:20 by shogura          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+NAME=a.out
+
 CC=gcc
 
-CFLAG=-g
+CFLAG= -Wall -Wextra -Werror
 
 NR=norminette -R CheckForbiddenSourceHeader
 
-SRCS=get_next_line.c get_next_line_utils.c
+SRCS=get_next_line.c get_next_line_utils.c main.c
+
+BONUS=get_next_line_bonus.c get_next_line_utils_bonus.c main.c
 
 all:${SRCS}
 	${CC} ${CFLAG} -D BUFFER_SIZE=42 $^
+
+bonus:${BONUS}
+	${CC} ${CFLAG}  -D BUFFER_SIZE=42 $^
 
 nr:${SRCS}
 	${NR} $^
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,107 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shogura <shogura@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/12 19:28:15 by shogura           #+#    #+#             */
+/*   Updated: 2022/04/20 11:09:51 by shogura          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	len;
+
+	len = 0;
+	while (*s++)
+		len++;
+	return (len);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	unsigned char	charset;
+
+	charset = (unsigned char)c;
+	while (*s)
+	{
+		if (*s == charset)
+			return ((char *)s);
+		s++;
+	}
+	if (c == 0)
+		return ((char *)s);
+	return (NULL);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	size_t	i;
+	size_t	len;
+	char	*str;
+
+	i = 0;
+	len = ft_strlen(s1);
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (str == NULL)
+		return (NULL);
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+char	*ft_strjoin(char *s1, char const *s2)
+{
+	char	*new_s;
+	size_t	len;
+	size_t	i;
+
+	i = 0;
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	len = ft_strlen(s1) + ft_strlen(s2);
+	new_s = malloc(sizeof(char) * (len + 1));
+	if (new_s == NULL)
+		return (NULL);
+	while ((len + 1) > 0)
+		new_s[len--] = '\0';
+	while (*s1)
+		new_s[i++] = *s1++;
+	while (*s2)
+		new_s[i++] = *s2++;
+	return (new_s);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	size_t	s_len;
+	char	*sub;
+
+	i = 0;
+	if (s == NULL)
+		return (NULL);
+	s_len = ft_strlen(s);
+	if (start >= s_len)
+		sub = (char *)malloc(sizeof(char) * 1);
+	else
+	{
+		if (len > ft_strlen(s + start))
+			len = ft_strlen(s + start);
+		sub = (char *)malloc(sizeof(char) * (len + 1));
+	}
+	if (sub == NULL)
+		return (NULL);
+	while (i < len && s[start] && start < s_len)
+		sub[i++] = s[start++];
+	sub[i] = '\0';
+	return (sub);
+}
